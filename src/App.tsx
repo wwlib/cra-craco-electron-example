61a25f13f635c956f663394c4c0ed54e9447f2b4
Andrew Rapo <andrew@wwlib.org>
2019-11-01 14:53:21 -0400

    updated react-scripts to 3.2 and converted App class to a function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import classes from './App.module.css';
+import React from 'react';
+import './App.css';
 
 import Logo from './Logo/Logo';
 import TestFs from './model/TestFs';
 
-class App extends Component {
-
-    render() {
-        return (
-          <div className={classes.App}>
-            <header className={classes.AppHeader}>
-              <Logo />
-              <textarea className={classes.TextArea} value={TestFs.getDirectoryListing()} readOnly rows={20} />
-            </header>
-          </div>
-        );
-    }
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <textarea className="App" value={TestFs.getDirectoryListing()} readOnly rows={20} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
